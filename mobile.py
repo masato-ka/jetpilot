cf06c36d25b578484416300908b95fd193265164
masato-ka <jp6uzv@gmail.com>
2019-09-16 23:23:45 -0700

    Add frozen model and modify mobile.py

--- mobile.py
+++ mobile.py
@@ -14,18 +14,20 @@ class MobileController(SingletonConfigurable):
     left_v = 0.0
     right_v = 0.0
 #    jitter = 1.111
-    max_radius = 60.0
-    speed = traitlets.Float(default_value=1.0).tag(config=True)
-    radius = traitlets.Float(default_value=1.0).tag(config=True)
+    max_radius = 50.0
+    speed = traitlets.Float(default_value=0.0).tag(config=True)
+    radius = traitlets.Float(default_value=0.0).tag(config=True)
     
-    def __init__(self, wheel_distance, robot: Robot, *args, **kwargs):
+    def __init__(self, wheel_distance, robot: Robot, pass_through=False,*args, **kwargs):
         super(MobileController, self).__init__(*args, **kwargs)
+        self.pass_through = pass_through
         self.wheel_distance = wheel_distance
         self.robot = robot
        
     @traitlets.observe('speed')
     def _observe_speed(self, change):
-        self.speed = self._input_disc(change['new'])
+        
+        self.speed = change['new'] if self.pass_through else self._input_disc(change['new'])
         self.controll()
         self.robot.set_motors(self.left_v, self.right_v)
     
@@ -45,24 +47,24 @@ class MobileController(SingletonConfigurable):
 
         negative = -1.0 if input < 0e-10 else 1.0
         input = abs(input)
-        if 0.01 < input <= 0.3:
+        if 0.01 < input <= 0.4:
             return negative * 0.3
-        elif 0.3 < input <= 0.5:
+        elif 0.4 < input <= 0.6:
             return negative * 0.5
-        elif 0.5 < input <= 0.7:
+        elif 0.6 < input <= 0.8:
             return negative * 0.7
-        elif 0.7 < input <= 1.0:
+        elif 0.8 < input <= 1.0:
             return negative * 1.0
         else:
             return 0.0
     
     def controll(self):
 
-        if self.radius < 0.0:
+        if self.radius < 0e-10:
             radius = (30 * self.radius) + (self.max_radius)
             self.left_v = ((radius-self.wheel_distance)*self.speed)/(self.max_radius-self.wheel_distance) if radius != 0.0 else self.speed
             self.right_v = ((radius+self.wheel_distance)*self.speed)/(self.max_radius+self.wheel_distance) if radius != 0.0 else self.speed
-        elif self.radius > 0.0:
+        elif self.radius > 0e-10:
             radius = (-30 * self.radius) + (self.max_radius)
             self.left_v = (radius+self.wheel_distance)*self.speed/(self.max_radius+self.wheel_distance)  if radius != 0.0 else self.speed
             self.right_v = (radius-self.wheel_distance)*self.speed/(self.max_radius-self.wheel_distance) if radius != 0.0 else self.speed
